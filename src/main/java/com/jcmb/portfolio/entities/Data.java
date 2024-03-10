81f53b292dbe2433573716025dc07709adaee182
ulioces <julmeca@gmail.com>
2024-03-10 16:54:21 +0100

    Add entity

---
+++ src/main/java/com/jcmb/portfolio/entities/Data.java
@@ -0,0 +1,32 @@
+package com.jcmb.portfolio.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "data")
+public class Data {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = true, unique = true)
+	private String phone;
+
+	@Column(nullable = true)
+	private String english_level;
+
+	@Column(nullable = true)
+	private String about_me;
+
+	@Column(nullable = true)
+	private String city;
+	
+	@Column(nullable = true)
+	private String country;
+	
+	@Column(nullable = true)
+	private String email;
+	
+	@Column(nullable = true)
+	private String linkedin;
+}
